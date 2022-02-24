d38c72a120f936c14633cffca544ac605af46fda
Marina <75340313+marina-t1123@users.noreply.github.com>
2022-02-24 23:30:02 +0900

    問診票テーブルの選手IDカラム作成のためのマイグレーションファイルの実行。

--- backend/database/migrations/2022_02_24_142553_add_athlete_user_id_column_to_medical_questionnaires_table.php
+++ backend/database/migrations/2022_02_24_142553_add_athlete_user_id_column_to_medical_questionnaires_table.php
@@ -14,7 +14,11 @@ class AddAthleteUserIdColumnToMedicalQuestionnairesTable extends Migration
     public function up()
     {
         Schema::table('medical_questionnaires', function (Blueprint $table) {
-            //
+            //整数型のathlete_user_idカラムにindexを指定して追加する
+            $table->unsignedBigInteger('athlete_user_id');
+            //外部キーをつける(参照する)には、foreignメソッドにathlete_user_idカラムを指定して、referencesメソッドにforeignメソッドで指定したカラム(athlete_user_idカラム)と
+            //紐づくカラムがどのテーブルのどのカラムかを指定する。今回は、athlete_usersテーブルのidカラムがmedical_questionnairesテーブルのathlete_user_idカラムと紐づいている。
+            $table->foreign('athlete_user_id')->references('id')->on('athlete_users');
         });
     }
 
@@ -26,7 +30,10 @@ class AddAthleteUserIdColumnToMedicalQuestionnairesTable extends Migration
     public function down()
     {
         Schema::table('medical_questionnaires', function (Blueprint $table) {
-            //
+            //外部キー制約を設定しているカラムの設定を解除する
+            $table->dropForeign('athlete_user_id');
+            //追加したカラムを削除する
+            $table->dropColumn('athlete_user_id');
         });
     }
 }
