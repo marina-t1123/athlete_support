a69c12dbb4121bc51d068d4f5a4a8ae6166d46e5
Marina <75340313+marina-t1123@users.noreply.github.com>
2022-03-07 23:54:03 +0900

    php artisan ui vue --authでエラー。app.jsとapp.cssが404。

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
